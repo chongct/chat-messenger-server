488d11525bd076791959b1d9213953e43937e355
chongct <chongchingtong@gmail.com>
2025-06-23 01:32:54 +0800

    Modify vercel.json to attempt to fix CORS

--- src/index.ts
+++ src/index.ts
@@ -2,32 +2,16 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
-import cors from 'cors';
 
 import routes from './routes';
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
-const whitelistCorsUrls =
-  process.env.NODE_ENV === 'development'
-    ? [process.env.FRONTEND_URI].concat('http://localhost:3000')
-    : [process.env.FRONTEND_URI];
-const corsOptions = {
-  origin: function (origin, callback) {
-    console.log('whitelistCorsUrls', whitelistCorsUrls, origin);
-    if (whitelistCorsUrls.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-};
 
 dotenv.config();
 
 app.use(bodyParser.json());
-app.use(cors(corsOptions));
 app.use('/', routes);
 
 (async () => {
