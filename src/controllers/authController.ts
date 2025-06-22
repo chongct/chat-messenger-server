ed8bb6dd43b7c537c9ab6372a20c120fa070d8e3
chongct <chongchingtong@gmail.com>
2025-06-22 21:33:57 +0800

    Setup database connection. Add auth/register route

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,10 +1,51 @@
 import { Request, Response } from 'express';
+import { validationResult, type FieldValidationError } from 'express-validator';
+import bcrypt from 'bcrypt';
+
+import { User } from '../models';
+
+const SALT_ROUNDS = 10;
 
 export const authTest = (req: Request, res: Response) => {
   res.send('Login test');
 };
 
 export const postLogin = (req: Request, res: Response) => {
-  console.log('req.body', req.body);
+  res.send('Login route');
+};
+
+export const postRegister = async (req: Request, res: Response) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    res.status(422).json({
+      error: errors.array().reduce((combinedError, error) => {
+        const { msg, path } = error as FieldValidationError;
+
+        return { ...combinedError, [path]: msg };
+      }, {}),
+    });
+
+    return;
+  }
+
+  const { firstName, lastName, email, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+  const user = new User({ firstName, lastName, email, password: hashedPassword });
+
+  try {
+    await user.save();
+    res.status(200).json({ success: true });
+  } catch (error) {
+    console.error(`Failed to register user: ${error}`);
+    res.status(500);
+  }
+};
+
+export const postRefreshToken = (req: Request, res: Response) => {
+  res.send('Login route');
+};
+
+export const postLogout = (req: Request, res: Response) => {
   res.send('Login route');
 };
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,10 +1,43 @@
 import { Router } from 'express';
+import { body, check } from 'express-validator';
 
-import { authTest, postLogin } from '../controllers';
+import { authTest, postLogin, postRegister, postRefreshToken, postLogout } from '../controllers';
+import { User } from '../models';
 
 const authRouter = Router();
 
 authRouter.get('/', authTest);
 authRouter.post('/login', postLogin);
+
+authRouter.post(
+  '/register',
+  [
+    check('email')
+      .isEmail()
+      .withMessage('Please enter a valid email')
+      .custom(async (email) => {
+        const user = await User.findOne({ email });
+
+        if (user) {
+          return Promise.reject('Email already in use');
+        }
+      })
+      .normalizeEmail(),
+    body('password').trim(),
+    body('confirmPassword')
+      .trim()
+      .custom((confirmPassword, { req }) => {
+        if (confirmPassword !== req.body.password) {
+          throw new Error('Password confirmation does not match password');
+        }
+
+        return true;
+      }),
+  ],
+  postRegister
+);
+
+authRouter.post('/refresh-token', postRefreshToken);
+authRouter.post('/logout', postLogout);
 
 export { authRouter };
