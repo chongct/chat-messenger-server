38b895345b60e628ced95e8cf880d8cae63c6bc1
chongct <chongchingtong@gmail.com>
2025-06-28 16:40:10 +0800

    Modify cookie sameSite settings

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,6 +3,6 @@ import { CookieOptions } from 'express';
 export const authCookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === 'production',
-  sameSite: 'none',
+  sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
   maxAge: 60 * 60 * 1000,
 } as CookieOptions;
