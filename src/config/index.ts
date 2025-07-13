bce32449a438ec11fb27976d42d8831429cd4a7e
chongct <chongchingtong@gmail.com>
2025-07-14 00:52:41 +0800

    Use sameSite lax for cookie settings

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,8 @@
 import { CookieOptions } from 'express';
 
 export const authCookieOptions = {
-  httpOnly: false, // disabled as ios third party cookies not working
+  httpOnly: true,
   secure: process.env.NODE_ENV === 'production',
-  sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+  sameSite: process.env.NODE_ENV === 'production' ? 'lax' : 'strict',
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 } as CookieOptions;
