0a5c7d8d6887475dddefa2e7ff6eac7e34b98092
chongct <chongchingtong@gmail.com>
2025-07-14 01:13:17 +0800

    Revert to use sameSite none for cookie settings

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,6 +3,6 @@ import { CookieOptions } from 'express';
 export const authCookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === 'production',
-  sameSite: process.env.NODE_ENV === 'production' ? 'lax' : 'strict',
+  sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 } as CookieOptions;
