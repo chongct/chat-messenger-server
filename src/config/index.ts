56995e27bc4e38ca0c0ca670147b7f9f6656ae4f
chongct <chongchingtong@gmail.com>
2025-07-21 02:06:26 +0800

    Reapply "Implement custom header csrf"
    This reverts commit 9b6b2722985fd533bbe70a317e360235fd60486d.

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,3 +6,11 @@ export const authCookieOptions = {
   sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 } as CookieOptions;
+
+export const csrfCookieOptions = {
+  httpOnly: false,
+  secure: process.env.NODE_ENV === 'production',
+  sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+} as CookieOptions;
+
+export const IS_COOKIE_DISABLED = true;
